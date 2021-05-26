1ef85ef5ac5b90979a5cfef02dc564a8a479ac4b
Eduardo Perez <edujpp1@gmail.com>
2021-05-26 15:17:26 -0300

    Creating a generic route to enable CORS on all the API routes

--- server/api/app.py
+++ server/api/app.py
@@ -13,6 +13,11 @@ _allow_origin = '*'
 _allow_methods = 'PUT, GET, POST, DELETE, OPTIONS'
 _allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
 
+def _enable_cors():
+    """ Add headers to enable CORS """
+    response.headers['Access-Control-Allow-Origin'] = _allow_origin
+    response.headers['Access-Control-Allow-Methods'] = _allow_methods
+    response.headers['Access-Control-Allow-Headers'] = _allow_headers
 
 @app.hook('before_request')
 def _before_request():
@@ -20,11 +25,11 @@ def _before_request():
 
 @app.hook('after_request')
 def _after_request():
-    '''Add headers to enable CORS and close db connection'''
-
-    response.headers['Access-Control-Allow-Origin'] = _allow_origin
-    response.headers['Access-Control-Allow-Methods'] = _allow_methods
-    response.headers['Access-Control-Allow-Headers'] = _allow_headers
-
+    _enable_cors()
     if not db.is_closed():
         db.close()
+
+@app.route('/<:re:.*>', method=['OPTIONS'])
+def _enable_cors_generic_route():
+    """ Add OPTIONS method to all routes to enable CORS """
+    _enable_cors()
